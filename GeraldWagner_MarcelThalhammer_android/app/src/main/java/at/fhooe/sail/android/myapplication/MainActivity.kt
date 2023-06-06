0a58fe76404dd0637ca1a5bb4c00412b4c0d43a2
Gerald <gerald.wagner-mistlberger@outlook.com>
2023-06-06 14:30:44 +0200

    Member 1 commit

--- GeraldWagner_MarcelThalhammer_android/app/src/main/java/at/fhooe/sail/android/myapplication/MainActivity.kt
+++ GeraldWagner_MarcelThalhammer_android/app/src/main/java/at/fhooe/sail/android/myapplication/MainActivity.kt
@@ -3,7 +3,10 @@ package at.fhooe.sail.android.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+const val TAG: String="Gerald woah do"
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
